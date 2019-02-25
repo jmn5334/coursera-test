e9b329e6e06d2dfd98bceae691ed334bae94d6da
Jacob Newcomer <jmnewc4@gmail.com>
2019-02-24 19:22:50 -0500

    A4: Gets all assignment 4 requirements working including bonus.

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -33,16 +33,16 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
-
+  for(var i = 0; i < names.length; i++) {
+    name = names[i]
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -50,16 +50,52 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
+    var firstLetter = name.charAt(0).toLowerCase();
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
+    if (firstLetter === "j") {
+      byeSpeaker.speak(name);
+    } else {
+      helloSpeaker.speak(name);
+    }
   }
-}
+
+  // JHU extra requirements
+  function getGreeting (name) {
+    var firstLetter = name.charAt(0).toLowerCase();
+    var greeting;
+    if (firstLetter === "j") {
+      greeting = byeSpeaker.speakSimple(name);
+    } else {
+      greeting = helloSpeaker.speakSimple(name);
+    }
+    return greeting;
+  }
+
+  namesWithGreetings = names.map(x => getGreeting(x));
+  for(var i = 0; i < names.length; i++) {
+    console.log(namesWithGreetings[i]);
+  }
+
+  // Bonus/optional requirements
+  var greetingArrays = {
+    hello: [],
+    bye: []
+  };
+  greetingArrays = names.reduce(function (accumulator, currentValue) {
+    accumulator.hello.push(helloSpeaker.speakSimple(currentValue));
+    accumulator.bye.push(byeSpeaker.speakSimple(currentValue));
+    return accumulator;
+  }, {hello: [], bye: []});
+  for(var i = 0; i < greetingArrays.hello.length; i++) {
+    console.log(greetingArrays.hello[i]);
+  }
+  for(var i = 0; i < greetingArrays.bye.length; i++) {
+    console.log(greetingArrays.bye[i]);
+  }
+  
+})();
